14acd074784a04c8d8b1702239c14e4d62fdbd7d
Mikhail Meluzov <meluzovm@gmail.com>
2018-01-09 11:42:32 +0000

    adde MySQL in provisioning

--- galera/provision.sh
+++ galera/provision.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-#ln -s /usr/bin/nodejs /usr/bin/node
 
+
+
+#Install MariaDB
+yum list installed MariaDB-server&>/dev/null || {
+yum -y install MariaDB-server MariaDB-client galera
 #Add repo dor MariaDB as simlink
 rm -f /etc/yum.repos.d/MariaDB.repo
 ln -s /vagrant/MariaDB.repo /etc/yum.repos.d/MariaDB.repo
-
-#Install MariaDB
- yum list installed MariaDB-server&>/dev/null || {
-yum -y install MariaDB-server MariaDB-client galera
 }
 #Install Ansible
- yum list installed ansible&>/dev/null || {
+yum list installed ansible&>/dev/null || {
 yum -y install epel-release
 yum -y install ansible
 }
 
-systemctl start mariadb	
+#Disable SELinux for MySQL
+#semanage permissive -a mysqld_t
 
-#command hubot -v &>/dev/null || {
-#npm install -g hubot coffee-script}
+#Change properties
+rm /etc/my.cnf 
+ln -s /vagrant/my.cnf /etc/my.cnf
+
+#Start MySQL service
+#systemctl start mariadb
+
+#Firewall Configuration
+#iptables --append INPUT --protocol tcp --source 192.168.33.20 --jump ACCEPT
+#iptables --append INPUT --protocol tcp --source 192.168.33.30 --jump ACCEPT
+#service save iptables
+
+#Change SSH Settings
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
