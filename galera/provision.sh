78deff3f080d3317595cb56a02fbf4ac6dc3c4cb
Mikhail Meluzov <meluzovm@gmail.com>
2018-01-10 15:38:19 +0000

    Galera Cluster config is ready

--- galera/provision.sh
+++ galera/provision.sh
@@ -4,32 +4,39 @@
 
 #Install MariaDB
 yum list installed MariaDB-server&>/dev/null || {
-yum -y install MariaDB-server MariaDB-client galera
-#Add repo dor MariaDB as simlink
-rm -f /etc/yum.repos.d/MariaDB.repo
+# rm -f /etc/yum.repos.d/MariaDB.repo
 ln -s /vagrant/MariaDB.repo /etc/yum.repos.d/MariaDB.repo
+yum -y install MariaDB-server MariaDB-client galera
 }
+
 #Install Ansible
 yum list installed ansible&>/dev/null || {
 yum -y install epel-release
 yum -y install ansible
 }
 
+#Install vim 
+yum list installed vim>/dev/null || {
+yum -y install vim
+}
+
+#Install net-tools
+yum list installed net-tools>/dev/null || {
+yum -y install net-tools
+}
+
+#Change SSH Settings
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+
 #Disable SELinux for MySQL
-#semanage permissive -a mysqld_t
+setenforce 0
 
 #Change properties
-rm /etc/my.cnf 
-ln -s /vagrant/my.cnf /etc/my.cnf
-
-#Start MySQL service
-#systemctl start mariadb
+cp /vagrant/galera.cnf /etc/my.cnf.d/
 
 #Firewall Configuration
 #iptables --append INPUT --protocol tcp --source 192.168.33.20 --jump ACCEPT
 #iptables --append INPUT --protocol tcp --source 192.168.33.30 --jump ACCEPT
 #service save iptables
 
-#Change SSH Settings
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-systemctl restart sshd
